3972a388ed2b1274d4311ade620f192feb79aaa6
Vector Le <vectorle@protonmail.com>
2019-06-27 21:38:02 -0700

    first commit

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+    return(
+        <header>
+            <img src="https://pre14.deviantart.net/1226/th/pre/f/2015/148/0/5/sad_pepe__feels_bad_man__vector_by_hirussai-d8uq43y.png"
+                 alt="sadpepe"
+            />
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header
+
